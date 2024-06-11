4f4321b18827e2dfdb761114ca10fe867d0b0104
Ariel-Tintore <ariielmay27@gmail.com>
2024-06-11 12:27:51 -0300

    Algumas variáveis

--- estudo.py
+++ estudo.py
@@ -1,6 +1,33 @@
-print("Hello World")
+print("Iniciando os estudos")
 
-idade = int(input("Qual sua idade? "))
-if idade >=18:
-     print("Você é maior de idade ")
-else: print("Você é menor de idade ")
+var1 = 12
+var2 = 30
+var3 = var1 + var2
+
+print(var3)
+
+var3 = var3 / 2
+
+print(var3)
+
+num1 = int(input("Digite um número a seguir: "))
+dobro = 2*num1
+
+print(dobro)
+
+x = 10
+y = 4.2
+
+num = float(input("Digite um número a seguir: "))
+
+# Fazendo com que o output seja o mesmo nos dois casos
+print(num > x*y, num <= x + y, num*y != x*y)
+print(num == x*y, num*y == x*y, y > x + num)
+
+x = 4.2
+
+y = 10
+
+z = "42"
+
+not (((x * y == z) and not (x < y)) or y % 2 == 0)
