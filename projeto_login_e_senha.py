79d2dab3b8aad456598d88f49019ef7115bb6cf1
Ariel-Tintore <ariielmay27@gmail.com>
2024-06-12 09:14:06 -0300

    Alterando nome do projeto

---
+++ projeto_login_e_senha.py
@@ -0,0 +1,42 @@
+# Banco de dados de usuários e senhas
+usuarios = []
+senhas = []
+
+def criar_conta():
+    usuario = input("Digite o nome de usuário: ")
+    senha = input("Crie uma senha: ")
+
+    # Armazenando o usuário e senha na lista
+    usuarios.append(usuario)
+    senhas.append(senha)
+    print("\nConta criada com sucesso!")
+
+def login():
+    usuario = input("Digite seu nome de usuário: ")
+    senha = input("Digite sua senha: ")
+
+    # Verificando se o usuário e senha correspondem
+    if usuario in usuarios and senha == senhas[usuarios.index(usuario)]:
+        print("Login bem-sucedido!")
+    else:
+        print("\nNome de usuário ou senha incorretos.")
+
+# Programa principal
+while True:
+    print("\nSelecione a opção desejada:")
+    print("1 - Criar conta")
+    print("2 - Fazer login")
+    print("3 - Sair")
+
+    opcao = input("Opção: ")
+
+    if opcao == "1":
+        criar_conta()
+    elif opcao == "2":
+        login()
+    elif opcao == "3":
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
+
+print("Programa encerrado.")
