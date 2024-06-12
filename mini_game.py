21d870a6ebc8219afc3dbfb9ed8954ff11403403
Ariel-Tintore <ariielmay27@gmail.com>
2024-06-12 14:36:56 -0300

    Agora ficou pronto!

--- mini_game.py
+++ mini_game.py
@@ -9,17 +9,19 @@ contagem_de_chances = 0
 while numero_do_usuario != numero_aleatorio:
     contagem_de_chances += 1  # Adiciona a contagem de chances 
 
-    if contagem_de_chances > 3:
-        print("\nAcabou suas chances ")
+    if contagem_de_chances == 5:
+        print("\nAcabou suas chances!")   
     elif numero_do_usuario < numero_aleatorio:
         print("\nUm pouco mais")
     else:
         print("\nUm pouco menos")
         
     # Solicita uma nova tentativa ao usuário
-    numero_do_usuario = int(input("Tente novamente: "))
+    numero_do_usuario = int(input("\nTente novamente: "))
 
 # Adiciona a contagem para a tentativa correta
 contagem_de_chances += 1
-
-print("Você acertou, o número era: ", numero_aleatorio, "Você precisou de ", contagem_de_chances, "tentativas. ")
+if numero_do_usuario == numero_aleatorio and contagem_de_chances == 1:
+        print("\nVocê é a mãe diná!! Acertou de primeira ")
+        
+print("Você acertou, o número era:", numero_aleatorio, "Você precisou de ", contagem_de_chances, "tentativas. ")
