e6bd02a93e5669fde2eeaf4349b4b10afd3c7d2c
Ariel-Tintore <ariielmay27@gmail.com>
2024-06-12 13:59:30 -0300

    aperfeiçoando o código

--- mini_game.py
+++ mini_game.py
@@ -4,12 +4,22 @@ numero_aleatorio = random.randint(1, 10)
 
 numero_do_usuario = int(input("Tente adivinhar o número de 1 a 10: "))
 
-while numero_do_usuario != numero_aleatorio:
-    if numero_do_usuario < numero_aleatorio:
-        print("\nUm pouco mais ")
-    else:
-        print("\nUm pouco menos ")
-    # Solicita uma nova tentativa ao usuário
-    numero_do_usuario = int(input("\nTente novamente: "))
+contagem_de_chances = 0
 
-print("Você acertou, o número era: ", numero_aleatorio)
+while numero_do_usuario != numero_aleatorio:
+    contagem_de_chances += 1  # Adiciona a contagem de chances 
+
+    if contagem_de_chances > 3:
+        print("\nAcabou suas chances ")
+    elif numero_do_usuario < numero_aleatorio:
+        print("\nUm pouco mais")
+    else:
+        print("\nUm pouco menos")
+        
+    # Solicita uma nova tentativa ao usuário
+    numero_do_usuario = int(input("Tente novamente: "))
+
+# Adiciona a contagem para a tentativa correta
+contagem_de_chances += 1
+
+print("Você acertou, o número era: ", numero_aleatorio, "Você precisou de ", contagem_de_chances, "tentativas. ")
